35414eb75553f8573cfd98b51f25e5714d3078b5
ryan <rewong03@gmail.com>
2020-12-11 11:19:14 -0600

    New 404 page

--- publish_site.sh
+++ publish_site.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo Pushing hugo-coder with message $1...
+
+cd themes/hugo-coder
+git add --all
+git commit -m "$1"
+git push origin master
+cd ../..
+
 hugo
 
 echo Pushing rewong03.github.io with message $1...
